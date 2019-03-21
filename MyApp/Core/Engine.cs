a3bdba7dfed7a41c3e3f4dee7d3beb4f91436548
Stefan Konakchiev <39467327+StefanKonakchiev@users.noreply.github.com>
2019-03-22 00:59:01 +0200

    Add files via upload

---
+++ MyApp/Core/Engine.cs
@@ -0,0 +1,39 @@
+﻿using MyApp.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyApp.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly IServiceProvider provider;
+
+        public Engine(IServiceProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string[] inputArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+                var commandIntepreter = this.provider.GetService<ICommandInterpreter>();
+                string result = commandIntepreter.Read(inputArgs);
+
+                Console.WriteLine(result);
+
+                //TODO add try catch block
+            }
+
+
+
+        }
+    }
+}
---
+++ MyApp/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Core;
+using MyApp.Core.Contracts;
+using MyApp.Data;
+using System;
+
+namespace MyApp
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //database
+            //command pattern
+            //DI
+            //DTOs
+            //Service Layer
+
+
+            IServiceProvider services = ConfigureServices();
+
+            IEngine engine = new Engine(services);
+            engine.Run();
+
+
+            MapperConfiguration mapperConfiguration = new MapperConfiguration();
+
+            //mapperConfiguration.Mapper.CreateMappedObject
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<MyAppContext>(db => 
+                db.UseSqlServer("Server=.\\SQLEXPRESS;Database=MySpecialApp;Integrated Security=True"));
+
+            serviceCollection.AddTransient<ICommandInterpreter, CommandInterpreter>();
+            serviceCollection.AddTransient<Mapper>();
+
+            //Da si gi vidq tiq dvete
+            //serviceCollection.AddSingleton<Mapper>();
+            //serviceCollection.AddScoped<Mapper>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}
